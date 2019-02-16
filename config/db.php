1608029a3216807bde3335cc81f1e351225b2a19
calojad <cal-107@hotmail.com>
2019-02-16 00:14:41 -0500

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,7 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'calojad');//Usuario de tu base de datos
+define('DB_PASS', 'cald2253');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'cal_facturacion');//Nombre de la base de datos
+?>
